a35f83e3ba972ffc7441aa4719ee51d9d0672133
Mohammed Aljaroudi <geekm110@icloud.com>
2020-12-03 10:38:12 -0800

    Search, Grids, CSS, More

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,29 +1,29 @@
-const get_cust_row = (comer) => {
-    return `<tr><td>${comer.address}</td><td>Some Info</td></tr>`
+var table_items = [];
+
+
+const generate_row = (obj) => {
+    return `<tr><td>${obj.address}</td><td>Some Info</td></tr>`
 }
 
-const get_truc_row = (truck) => {
-    return `<tr><td>${truck.id}</td><td>Some Info</td></tr>`
-}
-
-$('button.tab:not(.selected)').click( () => {
-    $('.table-small tbody').html('<tr><td colspan="2">NOTHING FOUND.</td></tr>');
-    
+$('button.tab').click( () => {
+    set_table_rows([]);
 });
 
 /*
 Note this does NOT run synchronously
 */
-
 $('#btn_comers').click( () => {
     $('#btn_comers').addClass('selected');
     $('#btn_trucks').removeClass('selected');
 
     $.getJSON('./assets/test_data.json', json => {
-        $('.table-small tbody').empty();
+
         const {customers, trucks} = json;
-        const rows = customers.map(get_cust_row);
-        $('.table-small tbody').append(rows);
+        const rows = customers.map(generate_row);
+        set_table_rows(rows);
+
+        // Global variable
+        table_items = customers;
     });
 });
 
@@ -32,11 +32,34 @@ $('#btn_trucks').click( () => {
     $('#btn_comers').removeClass('selected');
 
     $.getJSON('./assets/test_data.json', json => {
-        $('.table-small tbody').empty();
+        
         const trucks = json.trucks;
-        const rows = trucks.map(get_truc_row);
-        $('.table-small tbody').append(rows);
+        const rows   = trucks.map(generate_row);
+        set_table_rows(rows);
+
+        // Global variable
+        table_items  = trucks;
     });
 });
 
+$('#search-bar').keyup( () => {
 
+    const input   = $('#search-bar').val().toUpperCase();
+
+    const results = table_items.filter(item => item.address.toUpperCase().includes(input) || item.name.toUpperCase().includes(input));
+
+    const rows    = results.map(generate_row);
+
+    set_table_rows(rows);
+
+    $('#h-title').html(input.length > 0 ? `${results.length} results for "${input}"` : "Address");
+});
+
+function set_table_rows(rows) {
+    if (rows.length == 0) {
+        $('.table-small tbody').html('<tr><td colspan="2">NOTHING FOUND.</td></tr>');
+    } else {
+        $('.table-small tbody').empty();
+        $('.table-small tbody').append(rows);
+    }
+}
