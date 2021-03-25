a357eff1874c7119f49677ef769f8f41b73f6c63
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2021-03-25 13:34:08 -0700

    Updated Lift page

--- assets/js/lift.js
+++ assets/js/lift.js
@@ -8,19 +8,31 @@ const generate_card = (card) => {
     return `<div class="info-card"><span>${card}</div>`
 }
 
+const generate_video = (url) => {
+    return `<source src=".${url}" type="video/mp4"></source>`
+}
+
 $.getJSON('./assets/json/lift.json', json => {
     
     // extract lifts from JSON
+    const vid_url= json.vid_url;
     const address= json.address;
     let   cards  = json.cards;
     const history= json.previous_lifts;
-    $('#title').text(address);
     
+    // Set adddress title
+    $('#title').text(address.toUpperCase());
+    
+    // Populate Previous Lifts table
     const rows = Object.entries(history).map(generate_history);
     $('#history tbody').empty();
     $('#history tbody').append(rows);
 
+    // Generate info cards
     cards = Object.values(cards).map(generate_card);
     $('.grid-details').empty();
     $('.grid-details').append(cards);
+
+    // Set the video player's url
+    $('#vid_player').html(generate_video(vid_url));
 });
