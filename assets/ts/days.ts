dfecad54c14b8b9eeb7b97df0ec32fca877e6bfb
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2021-05-07 19:21:20 -0700

    Show simulated paths

--- assets/ts/days.ts
+++ assets/ts/days.ts
@@ -11,15 +11,17 @@ function add_day(route: string, sum_bins: number, det_bins: number, day: number)
     const perc = (det_bins / sum_bins * 100).toFixed(0);
     const btyp = ['🏪', '🏠'][Math.floor(Math.random() * 2)];
     const type = perc == '100' ? 'class="complete"' : '';
-    $('tbody').append(`<tr onclick="document.location = 'lifts.html?city_id=${param('city_id')}&route_id=${route}&day=${'Mar ' + day}';" ${type}><td><row>
-    <div  id="A">Mar ${day}</div>
+    $('tbody').append(`<tr onclick="document.location = 'lifts.html?city_id=${param('city_id')}&route_id=${route}&day=${'Sep ' + day}';" ${type}><td><row>
+    <div  id="A">Sep ${day}</div>
     <span id="B">${perc}%</span>
     <div  id="C">${btyp} Truck ${rand(1000, 9999)}</div>
     <span id="D">missed ${sum_bins - det_bins}</span>
     </row></td></tr>`);
 };
 
-const days: number[] = [...Array(10)].map(e => Math.random() * 32 | 1).sort((a, b) => a - b).reverse();
+const rand = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
+const days: number[] = [...Array(10)].map(_ => rand(8, 31)).sort((a, b) => a - b).reverse()
+// const days: number[] = [...Array(10)].map(e => Math.random() * 32 | 1).sort((a, b) => a - b).reverse();
 
 for (const day of days) {
     const id = rand(1000, 9999);
@@ -38,24 +40,17 @@ mapkit.init({
 
 var map = new mapkit.Map("map", { colorScheme: 'dark' });
 
-$.getJSON('http://35.227.154.149:8000/trucks/online_only/', response => {
-
-    const dvr_id = Object.keys(response)[Math.floor(Math.random() * Object.keys(response).length)];
-    const logs = response[dvr_id];
-
+$.getJSON('assets/data/map.geojson', response => {
     const style = new mapkit.Style({ lineWidth: 2, lineJoin: 'round', strokeColor: '#FFF' });
-    const date = logs.shift();
 
-    const len = logs.length;
+    const coo = response.geometry.coordinates;
 
-    for (var coo = [], i = 0; i < len - 1; i += 2) {
-        const lat: number = logs[i], lon: number = logs[i + 1];
-        coo.push(new mapkit.Coordinate(lat, lon));
-    }
+    const Coo = coo.map(entry => {
+        return new mapkit.Coordinate(entry[1], entry[0]);
+    })
 
-    const line = new mapkit.PolylineOverlay(coo, { style: style });
-    // map.showItems(annotations);
+    const line = new mapkit.PolylineOverlay(Coo, { style: style });
+
     map.addOverlays([line]);
     map.showItems([line]);
-
-});
+})
